d958fb2f83fa51a87beb21007742b0a92f94d09f
idan@r2net.com <Ih@019283>
2022-01-02 00:23:40 +0200

    init

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+
+export const Footer = ()=>{
+    return (
+        <footer>footer</footer>
+    );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+
+export const Header = ()=>{
+    return (
+        <header>header1</header>
+    );
+}
