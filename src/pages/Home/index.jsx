67cba2ecab86ce339fb7bfe72a65ae7022682093
idan@r2net.com <Ih@019283>
2022-01-04 21:18:10 +0200

    init

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,11 +8,11 @@ export const Home = ()=>{
             <h2>Home</h2>
             <h3>Authors:</h3>
             <ul>
-                <li>Idan Hakim</li>
-                <li>Oleg Rudoy</li>
-                <li>Yafit Fridman</li>
-                <li>Dor Nakash</li>
-                <li>Tomer</li>
+                <li>Idan Hakim.</li>
+                <li>Oleg Rudoy.</li>
+                <li>Yafit Fridman.</li>
+                <li>Dor Nakash.</li>
+                <li>Tomer.</li>
             </ul>
         </div>
     );
